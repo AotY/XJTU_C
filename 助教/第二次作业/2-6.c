e67eca6b3b7bcf81ebc3a341a86c68516f6c4bf7
LeonTao <694731929@qq.com>
2016-11-03 13:51:49 +0800

    Update 2-6.c

--- 助教/第二次作业/2-6.c
+++ 助教/第二次作业/2-6.c
@@ -7,6 +7,28 @@ a <= b< c< =N
 样例：
 10
 2
+
+也可以这样写
+int main()
+{  
+    int n;
+    scanf("%d", &n);
+    int i, j, k;
+    int s = 0;
+    for (i = 1; i <n;i++)
+    {
+        for (j = 2; j <n;j++)
+        {
+            for (k = j + 1; k <= n; k++)
+            {
+                if (k*k == i*i + j*j)
+                    s = s + 1;
+            }
+        }
+    }
+    printf("%d", s / 2);
+    return 0;
+}
 */
 
 #include <stdio.h>
